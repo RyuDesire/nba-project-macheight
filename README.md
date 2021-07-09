c12503a5500c74b79ea2d90d884f321e9e808202
Ryu Desire <cesarstiven2017@gmail.com>
2021-07-09 04:42:02 -0500

    [mach-eight-project] - c02 version 1.1 reducing quantity of data to show, writting README, and a few improves more

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 # MachEight
+---
+### How it works?
 
-Wanna see it on live? [Visit Now]
+    1. Whent the user write and integer & clicks on the look up button.
+    2. It's value is save in a variable **heigth** as integer
+    3. The input field is cleared
+    4. The **showData()** function is call with the value of **heigth** as a parameter 
+        **function showData()**
+        1. Capture the div with id *results* as a target in **result** variable
+        2. Clear is inner HTML.
+        3. As **showData()** 
 
+    1. This project use the fecth API to get the data of NBA players from this endpoint [NBA Players](https://mach-eight.uc.r.appspot.com/).
+    2. Converts it to JSON format
+
+
+## Live Demo
+
+    Wanna see it on live? [Visit Now](https://ryudesire.github.io/nba-project-macheight/ "Live Demo")
+
+## Get the source code
+
+    To download:
+      [Download](https://github.com/RyuDesire/nba-project-macheight/archive/refs/heads/main.zip)
+    
+    To Clone:
+      *NOTE: As a requirement to clone this repository Git must be installed in you PC*
+      Use you prefered command prompt and navigate to the folder where you want to donwload the copy of this repository
+      **Copy and paste the following code in your command prompt:**
+      *git clone https://github.com/RyuDesire/nba-project-macheight.git*
+
+---
+#### Things to improve
+   * Write the tests cases
+      * User's input should be a integer
+   * User can't press look up button without data in the input
+   * Solve issues with overload of data. example: input_data = 156 => result 6630
+      lazyload ? pagination ?
--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // 1. Get user input as integer
 const input = document.querySelector("#look-up").addEventListener("click", ()=> {
   const heigth = Number(document.querySelector("#height").value);
+  document.querySelector("#height").value = '';
   showData(heigth);
 });
 
@@ -19,14 +20,18 @@ const pairedPlayers = async (value) => {
   let players = [];
   for (let firstPlayer = 0; firstPlayer < data.length; firstPlayer++)
   {
-    for (let secondPlayer = firstPlayer + 1; secondPlayer < data.length; secondPlayer++) {
-      if(Number(data[firstPlayer].h_in) + Number(data[secondPlayer].h_in) === value)
-      {
-        
-        players.push([`${data[firstPlayer].first_name} ${data[firstPlayer].last_name}`,`${data[secondPlayer].first_name} ${data[secondPlayer].last_name}`])
+    let diference = value - Number(data[firstPlayer].h_in);
+    let matches = data.filter((player) => Number(player.h_in) === diference && data.indexOf(player) > firstPlayer);
+      if(matches){
+        matches.forEach((player) => players.push([`${data[firstPlayer].first_name} ${data[firstPlayer].last_name}`, `${player.first_name} ${player.last_name}`]))
       }
+    // for (let secondPlayer = firstPlayer + 1; secondPlayer < data.length; secondPlayer++) {
+    //   if(Number(data[firstPlayer].h_in) + Number(data[secondPlayer].h_in) === value)
+    //   {
+    //     players.push([`${data[firstPlayer].first_name} ${data[firstPlayer].last_name}`,
+    //     `${data[secondPlayer].first_name} ${data[secondPlayer].last_name}`])
+    //   }
     }
-  }
   return players;
 }
 
@@ -34,15 +39,21 @@ const showData = async (value) => {
   const result = document.querySelector('.results')
   result.innerHTML = "";
   const data = await pairedPlayers(value);
-  console.log(data)
   if(!data.length){
     result.innerHTML=`<li>Matches not found</li>`
   }
-  else{
-    data.map( element => result.innerHTML+=`<li>${element[0]} - ${element[1]}</li>`)
+  else if(data.length <= 250 ){
+    for(let i = 0; i < data.length; i++){
+      result.innerHTML+=`<li>${data[i][0]} - ${data[i][1]}</li>`
+    //data.map( element => result.innerHTML+=`<li>${element[0]} - ${element[1]}</li>`)
   }
-} 
- showData()
+}
+  else{
+    //pending inplementations
+    result.innerHTML=`<li>Data exceed the actual limits.<br>Check the console</li>`
+  }
+}
+showData();
 //CONSTRAINS:
 //1. Algorithm must be faster than O(n^2)
 //2. All edge cases should be handled
